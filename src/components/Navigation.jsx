0df84ce3346f7e16fa419b1388d71bfc0b32ddbe
Courtney Hampton <jaye0888@gmail.com>
2023-04-14 17:20:32 +0200

    Animations and Routes
    Set up NavBar with animations also fixed routes. User can navigate to each page.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,31 +1,74 @@
 import { useLocation } from "react-router-dom";
 import IAMLogo from "../assets/IAMLogo.png";
-import { BsArrowRightCircle } from "react-icons/bs";
+import { BsArrowRightCircle, BsArrowLeftCircle } from "react-icons/bs";
+import { useState } from "react";
 
 const Navigation = () => {
   const location = useLocation();
+  const [isOpen, setOpen] = useState(false);
+
+  const checkOpen = () => {
+    if (isOpen) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
+  };
 
   return (
-    <div id="navContainer">
+    <div id="navContainer" className={isOpen ? "showNav" : ""}>
       <div id="sideNavigation">
         <div id="logoBox">
           <img src={IAMLogo} alt="I Ate A Mech" />
         </div>
         <div id="IAteAMech">IAteAMech</div>
+        <hr style={{ width: isOpen ? "30%" : "" }} />
         <div id="locations">
-          <a href="">
-            <div>About</div>
+          <a href="/About" className={isOpen ? "navAnimation" : "navClosed"}>
+            <div
+              className={location.pathname === "/About" ? "selectedPage" : ""}
+            >
+              About
+            </div>
           </a>
-          <a href="">
-            <div>Contact Us</div>
+          <a
+            href="/ContactUs"
+            className={isOpen ? "navAnimation" : "navClosed"}
+            style={{ animationDelay: isOpen === false ? "0s" : "" }}
+          >
+            <div
+              className={
+                location.pathname === "/ContactUs" ? "selectedPage" : ""
+              }
+            >
+              Contact Us
+            </div>
           </a>
-          <a href="">
-            <div>Home</div>
+          <a
+            href="/"
+            className={isOpen ? "navAnimation" : "navClosed"}
+            style={{ animationDelay: isOpen === false ? "0s" : "" }}
+          >
+            <div className={location.pathname === "/" ? "selectedPage" : ""}>
+              Home
+            </div>
           </a>
         </div>
       </div>
-      <div id="arrowOut">
-        <BsArrowRightCircle size={36} />
+      <div
+        id="arrowOut"
+        onClick={() => {
+          checkOpen();
+        }}
+      >
+        {isOpen ? (
+          <BsArrowRightCircle size={36} className={isOpen ? "navOpened" : ""} />
+        ) : (
+          <BsArrowLeftCircle
+            size={36}
+            className={isOpen === false ? "navOpened" : ""}
+          />
+        )}
       </div>
     </div>
   );
--- src/components/views/MainPage.jsx
+++ src/components/views/MainPage.jsx
@@ -3,7 +3,7 @@ const MainPage = () => {
     <div id="mainPageContainer">
       <div id="terminal"></div>
       <div id="jayebotWelcome">
-        <div id="jayeBubble"></div>
+        <div id="jayeBubble">Welcome</div>
       </div>
     </div>
   );
