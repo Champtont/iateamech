b937239e689b1ff27037c36fdd7609afeaba19e6
Courtney Hampton <jaye0888@gmail.com>
2023-04-17 17:28:08 +0200

    Update to About Us and Main Page

--- src/components/views/AboutUs.jsx
+++ src/components/views/AboutUs.jsx
@@ -1,8 +1,28 @@
 const AboutUs = () => {
   return (
     <div id="container">
-      <div id="history"></div>
-      <div id="mission"></div>
+      <div id="history">
+        Today, The business world is one with the Internet. This means that all
+        businesses, no matter the size, have to move in this direction or face
+        the possibility of being forgotten, or even parish. Simply put, your
+        customers, your clients, your people... are unable to find you without
+        the help of the internet. Large companies have the means to employ tons
+        of computer scientists, data analysts, full-stack, frontend, and backend
+        developers... but small business is often left in the dust. This is
+        where IAteAMech comes into play. Founded in 2021, we wanted to create a
+        solution to keep small business competitive. IAteAMech is a company that
+        provides quality and affordability, while keeping our clients vision and
+        brand in mind.
+      </div>
+      <div id="mission">
+        We are a group of Freelancers who love to code and come up with unique
+        and innovative solutions that fit within the budget and time restraints
+        of our clients!
+      </div>
+      <p>
+        Reach out for a free consultation <a href="/ContactUs">here</a>! We can
+        learn about each other and we'll share our expertise with you!
+      </p>
       <div id="carouselTestimonials"></div>
     </div>
   );
--- src/components/views/MainPage.jsx
+++ src/components/views/MainPage.jsx
@@ -2,8 +2,8 @@ const MainPage = () => {
   return (
     <div id="mainPageContainer">
       <div id="terminal"></div>
-      <div id="jayebotWelcome">
-        <div id="jayeBubble">Welcome</div>
+      <div id="jayeBotWelcome">
+        <div class="jayeBubble">Welcome!</div>
       </div>
     </div>
   );
